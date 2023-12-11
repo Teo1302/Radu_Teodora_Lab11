431fe491e72670d40c82d8da07f23e27f214d9d9
Teo1302 <teodora.radu1302@yahoo.com>
2023-12-11 17:40:05 +0200

    Add project files.

---
+++ Radu_Teodora_Lab11/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Radu_Teodora_Lab11.Data;
+namespace Radu_Teodora_Lab11
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+        }
+    }
+}
